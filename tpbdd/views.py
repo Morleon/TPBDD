414775f276e2ed7ab7e715e99b69ddd7b843ae89
mpoire <poire.e1700917@etud.univ-ubs.fr>
2021-03-17 18:16:06 +0100

    méthode POST + affichage des données chiffrées par ordre de grandeur

--- tpbdd/views.py
+++ tpbdd/views.py
@@ -3,17 +3,24 @@ from tpbdd.models import DatabaseData
 #from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import loader
+from pyope.ope import OPE
+import random
 
 # Create your views here.
 def homepage(request):
-	data = DatabaseData.objects.order_by('-number')[:10]
+	data = DatabaseData.objects.order_by('number')[:10]
 	context = {'data': data}
 	#query_results = YourModel.objects.all()
 	return render(request, 'index.html', context)
 
 def insertion(request):
-	#return render("The database has been added")
-	return HttpResponse("The database has been initialised")
+	print(request.POST)
+	cipher = OPE(b'mDErfp0Arn+noGTR+p0P9CmGU8+KKenk8ff+6aHxPi0=')
+	for i in range(1, 10): 
+ 		value = random.randrange(1, 10000)
+ 		value = cipher.encrypt(value)
+ 		data_instance = DatabaseData.objects.create(key=i, number=value)
+	return render(request, "insertion.html")
 
 #def sender(request, data):
 #	return HttpResponse("Data send : %s" % data)
