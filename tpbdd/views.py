a6dd71781cbee986e4704bfda1e253a232c43097
mpoire <poire.e1700917@etud.univ-ubs.fr>
2021-03-18 09:06:34 +0100

    MAJ affichage, gestion du déchiffrement

--- tpbdd/views.py
+++ tpbdd/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from tpbdd.models import DatabaseData
-#from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import loader
 from pyope.ope import OPE
 import random
 
-# Create your views here.
 def homepage(request):
 	data = DatabaseData.objects.order_by('number')[:10]
-	context = {'data': data}
-	#query_results = YourModel.objects.all()
+	encrypted_data = DatabaseData.objects.values('key','number').order_by('number')
+	decrypted_data = {'key':'value'}
+	cipher = OPE(b'mDErfp0Arn+noGTR+p0P9CmGU8+KKenk8ff+6aHxPi0=')
+	for x in encrypted_data:
+		x["number"] = cipher.decrypt(x["number"])
+	context = {'data': data, 'encrypted_data': encrypted_data}
 	return render(request, 'index.html', context)
 
 def insertion(request):
@@ -21,6 +23,3 @@ def insertion(request):
  		value = cipher.encrypt(value)
  		data_instance = DatabaseData.objects.create(key=i, number=value)
 	return render(request, "insertion.html")
-
-#def sender(request, data):
-#	return HttpResponse("Data send : %s" % data)
